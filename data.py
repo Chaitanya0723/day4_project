035ee753283c5f799fefd70a7ad62c47233a5f05
Chaitanya Pawar <chaitanyapawarb@gmail.com>
2025-07-10 12:40:02 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,3 @@
-# data.py
-
 students = [
     {"id": 1, "name": "Tejas", "marks": {"math": 90, "science": 81, "english": 98}},
     {"id": 2, "name": "Rushi", "marks": {"math": 75, "science": 97, "english": 91}},
